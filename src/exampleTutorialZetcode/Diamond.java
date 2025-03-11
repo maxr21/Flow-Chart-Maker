253619a051251996963d5d5385bffcc53a657d95
Max Robinson <robinson.max@northeastern.edu>
2025-03-10 21:06:34 -0400

    copied

---
+++ src/exampleTutorialZetcode/Diamond.java
@@ -0,0 +1,103 @@
+package exampleTutorialZetcode;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Diamond extends flowItem{
+
+		//Starting X and Y coordinates of centre of shape
+		private int x = 175;
+		private int y = 449;
+		
+		//Gets updated immediately in draw()
+		private int[] diamX = {0, 0, 0, 0};
+		private int[] diamY = {0, 0, 0, 0};
+		
+		//distance of vertices from mouse (the bigger this is the bigger the shape)
+		private int offsetX = 40;
+		private int offsetY = 40;
+		public String textObj = "";
+		
+		public Diamond(JPanel panel){
+			super(panel);        
+	        textField.setBackground(Color.ORANGE);
+	        textField.setForeground(Color.BLACK);
+		}
+		
+		public void draw(Graphics g) {
+			super.draw(g);
+			Graphics2D g2d = (Graphics2D) g;
+			
+			g2d.setColor(Color.ORANGE);
+			
+			//Moving the x vertices of Diamond (offset is the distance from the middle of the diamond)
+	    	diamX[0] = x - offsetX;
+	    	diamX[1] = x;
+	    	diamX[2] = x + offsetX;
+	    	diamX[3] = x;
+	    	
+	    	//Moving the y vertices of Diamond (offset is the distance from the middle of the diamond)
+	    	diamY[0] = y;
+	    	diamY[1] = y + offsetY;
+	    	diamY[2] = y;
+	    	diamY[3] = y - offsetY;
+
+	        Polygon diam1 = new Polygon(diamX, diamY, 4);       
+	        g2d.fillPolygon(diam1);		
+			
+	        textField.setBounds(x - (textFieldSize-20)/2, y - 40/2, textFieldSize - 20, 40);
+	        
+		}
+		
+		public void imageify(Graphics g, BufferedImage flowchartImg) {
+	        //BufferedImage flowchartImg = new BufferedImage(1800, 900, BufferedImage.TYPE_INT_RGB);
+
+	    	Graphics2D g2d = flowchartImg.createGraphics();
+	    	g2d.setColor(Color.ORANGE);
+			g2d.fillPolygon(diamX, diamY, 4);
+		}
+		
+		// mutator and accessor methods for diamond: x, y, x vertices, y vertices
+		public void setX(int x) {
+			this.x = x;
+		}
+		
+		public int getX(){
+			return x;
+		}
+		
+		public void setY(int y) {
+			this.y = y;
+		}
+		
+		public int getY(){
+			return y;
+		}
+		
+
+		public int getW() {
+			//calculates width
+			int w =  getLargest(diamX) - getSmallest(diamX);
+			return w;
+		} 
+		public void setW(int w) {
+			offsetX = offsetX + w;
+		}
+		
+		public int[] getXVertices() {
+			return diamX;
+		}
+		
+		public int[] getYVertices() {
+			return diamY;
+		}
+		public JTextField getText() {
+			return this.textField;
+		}
+}
---
+++ src/exampleTutorialZetcode/Frame.java
@@ -0,0 +1,139 @@
+package exampleTutorialZetcode;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.util.Objects;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+public class Frame extends JFrame {
+	
+	static exampleTutorialZetcode.Board board;
+	//size of window
+	public final static int width = 1800;
+	public final static int height = 900;
+
+	
+    public Frame() {      
+        initUI();
+    }
+    
+    private void initUI() {
+    	
+    	//window settings
+        this.setResizable(true);        
+        this.pack();
+        this.setTitle("FlowChart");
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(width, height);
+        
+        Frame.board = new Board();
+        
+        //this allows me to choose where to put the buttons
+        board.setLayout(null);
+        
+        Icon exportIcon = new ImageIcon(
+            Objects.requireNonNull(getClass().getClassLoader().getResource("exportIcon.png")));
+        //RectBox button image
+        Icon rectIcon = new ImageIcon(
+            Objects.requireNonNull(getClass().getClassLoader().getResource("rectButtonIcon.png")));
+        //Diamond button image
+        Icon diamondIcon = new ImageIcon(Objects.requireNonNull(
+            getClass().getClassLoader().getResource("diamondButtonIcon.png")));
+        //parallelogram button image
+        Icon slantSqrIcon = new ImageIcon(Objects.requireNonNull(
+            getClass().getClassLoader().getResource("slantSqrButtonIcon.png")));
+        
+        JButton exportB = new JButton(exportIcon);
+        exportB.setContentAreaFilled(false);
+        exportB.setFocusPainted(false);
+        exportB.setBorderPainted(false);
+        
+        exportB.setBounds(1250, 20, 200, 100);
+        
+        exportB.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+            	
+            	board.setImageMake(true);
+            	System.out.println("imageify = true");
+            }
+        });  
+        
+        board.add(exportB);
+        
+        JButton rectButton = new JButton(rectIcon);
+        rectButton.setContentAreaFilled(false);
+        rectButton.setFocusPainted(false);
+        rectButton.setBorderPainted(false);
+        
+        rectButton.setBounds(25, height/3 - height/6 - 25, 100, 50); //rect height is 50, 50/2 = 25
+        /*i want each button to occupy a different third of the screen, hence height/3 will make button occupy first third
+          -height/6 is so that the button is in the center of this third of the screen rather than at the top     
+        */
+
+        //listener for when pushing the button
+        rectButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+            	//adds a rectBox object to board when pushed
+            	board.getList().add(new RectBox(board));
+
+            }
+        });  
+        
+        //Diamond Button
+        JButton diamondButton = new JButton(diamondIcon);
+        diamondButton.setContentAreaFilled(false);
+        diamondButton.setFocusPainted(false);
+        diamondButton.setBorderPainted(false);
+        diamondButton.setBounds(24, 2*height/3 - height/6 - 40, 80, 80); //diam height is 80, 80/2 = 40
+        
+        //listener for when pushing the button
+        diamondButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+            	//adds a Diamond object to board when pushed
+            	board.getList().add(new Diamond(board));
+              
+              }
+        });  
+        
+        //SlantSqr button
+        JButton SlantSqrButton = new JButton(slantSqrIcon);
+        SlantSqrButton.setContentAreaFilled(false);
+        SlantSqrButton.setFocusPainted(false);
+        SlantSqrButton.setBorderPainted(false);
+        SlantSqrButton.setBounds(25, height - height/6 - 20, 100, 25); //slant height is 40, 40/2 = 20
+        
+        //listener for when pushing the button
+        SlantSqrButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+            	//adds a SlantSqr object to board when pushed
+            	board.getList().add(new SlantSqr(board));
+              
+            }
+        });  
+        
+        //all buttons added to board
+        board.add(rectButton);
+        board.add(diamondButton);
+        board.add(SlantSqrButton);
+        
+        //adds newly added flowItems textbox to board
+        for (int i = 0; i < board.getList().size()-1; i++) {
+        	board.add(board.getList().get(i).getText());
+        }
+        
+        this.add(board);
+    }
+    
+    public static void main(String[] args) {
+        
+        Frame ex = new Frame();
+
+        ex.setVisible(true);
+    }
+}
